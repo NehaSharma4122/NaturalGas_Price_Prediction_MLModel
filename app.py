37741b59069e2494a1578324851bff775f61bd5a
Neha <rmneha1234@gmail.com>
2025-07-28 04:50:15 +0530

    final

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def y_predict():
     return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
